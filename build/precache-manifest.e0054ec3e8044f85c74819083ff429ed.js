bd8b718ba14e8ac0d1eb564e5440891a46cb5924
Yaser Meeran <yasher0304@gmail.com>
2022-10-17 22:51:53 +0400

    Added UI code

---
+++ build/precache-manifest.e0054ec3e8044f85c74819083ff429ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91242ee4a597c1a08e3f901cf300ea59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b07b7c80fd955fb3159",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "74aec926e1517ad79e0c",
+    "url": "/static/js/2.26d1656d.chunk.js"
+  },
+  {
+    "revision": "eca373d6688a5d5efead34244f081e8b",
+    "url": "/static/js/2.26d1656d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b07b7c80fd955fb3159",
+    "url": "/static/js/main.d31aab1b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d31aab1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
